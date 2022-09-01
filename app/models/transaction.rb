9d848376b272499116da42055c4a6d1a770354e5
Lucas Falbo <95.lucas@gmail.com>
2022-09-01 01:51:15 -0300

    Refactor main validation for fees atribute of Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,12 @@ class Transaction < ApplicationRecord
   belongs_to :account
 
   validates :value, numericality: { greater_than: 0 }
-  validates :fees, numericality: { greater_than: 0 }
+  validates :fees, numericality: { greater_than_or_equal_to: 0 }
   validates :kind, inclusion: {in: %w(withdraw deposit transfer),
     message: "%{value} is not a valid kind of transaction" }
+
+  before_validation do
+    self.fees = 0 if self.fees.nil?
+  end
 
 end
