16c0cacccc38f635037b5d9aa79b79907acc5098
Lucas Falbo <95.lucas@gmail.com>
2022-09-01 01:40:10 -0300

    Add basic validation to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :account
+
+  validates :value, numericality: { greater_than: 0 }
+  validates :fees, numericality: { greater_than: 0 }
+  validates :kind, inclusion: {in: %w(withdraw deposit transfer),
+    message: "%{value} is not a valid kind of transaction" }
+
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'basic validation' do
+
+    describe 'value' do
+
+      it 'should only be positive' do
+        acc = Account.create
+        trans = acc.transactions.create(value: -30,fees: 30,kind: "withdraw")
+        expect(trans.invalid?).to eql(true)
+      end
+
+    end
+
+    describe 'fee' do
+
+      it 'should only be positive' do
+        acc = Account.create
+        trans = acc.transactions.create(value: 30,fees: -30,kind: "withdraw")
+        expect(trans.invalid?).to eql(true)
+      end
+
+    end
+
+    describe 'kind' do
+
+      it 'should only be withdraw, deposit or transfer' do
+        acc = Account.create
+        trans = acc.transactions.create(value: 30,fees: 30,kind: "test")
+        expect(trans.invalid?).to eql(true)
+      end
+
+    end
+  end
+
+  context 'fee calculation' do
+
+  end
+
 end
