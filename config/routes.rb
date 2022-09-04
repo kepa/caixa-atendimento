523c0818fe751ab468ef7599fe45adfb9a340996
Lucas Falbo <95.lucas@gmail.com>
2022-09-04 20:43:12 -0300

    Add basic user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
+  resources :users
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+
   get 'static/home'
   get 'static/deposit'
   get 'static/withdraw'
   get 'static/transfer'
-  resources :accounts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  resources :accounts
+
   root "accounts#index"
 end
