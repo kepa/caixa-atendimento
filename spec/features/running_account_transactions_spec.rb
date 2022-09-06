8db9fb5d1b1d34ded8ca94ecf2c37cf0d1c838d5
Lucas Falbo <95.lucas@gmail.com>
2022-09-06 13:55:55 -0300

    Add integration tests to transfer transactions

--- spec/features/running_account_transactions_spec.rb
+++ spec/features/running_account_transactions_spec.rb
@@ -33,4 +33,23 @@ RSpec.describe 'Running all major transactions', type: :feature do
     expect(page).to have_content('withdraw')
   end
 
+  scenario 'create an account, deposit and transfer money' do
+    visit login_path
+    fill_in 'Username', with: 'test'
+    fill_in 'Password', with: 'test'
+    click_on 'Login'
+    click_on 'Abrir uma nova conta'
+    visit welcome_path
+    click_on 'Abrir uma nova conta'
+    click_on 'Depositar'
+    fill_in 'account[deposit_value]', with: '40'
+    click_on 'Update Account'
+    click_on 'Transferir'
+    fill_in 'account[transfer_value]', with: '20'
+    fill_in 'account[dest_account]', with: Account.first.id
+    click_on 'Transferir'
+    expect(page).to have_content('transfer')
+    expect(page).to have_content('fee')
+  end
+
 end
